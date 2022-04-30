3612ab9be726559a015301a3ad7a43aea839c5d9
LisaMolloy5 <molloylisa5@gmail.com>
2022-04-30 17:03:25 +0000

    Create game welcome text

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
+### Tutorial followed and changed for this project - https://www.youtube.com/watch?v=xRlN8CFJwAM&t=175s
 import random
+
+print("-------------------------------------------------------------------")
+print("Welcome to Water Earth Fire")
+print("A rock, paper, scissors type game based on Avatar The Last Airbender.\n Use your abilities to defeat your oponent.")
+print("--------------------------------------------------------------------")
+
+def playGame():
+    """
+    Function to create game combinations and playr input.
+    """
+    playerChoice = input("Make your choice,  Water Earth or Fire: ")
+    if (playerChoice != "Water" or playerChoice != "Earth" or playerChoice != "Fire"):
+        return False
+        print("Invalid choice. Please try again.")
+
+    computerChoice = random.choice(['Water', 'Earth', 'Fire'])
+
+    if player 
+
+    
 
 
