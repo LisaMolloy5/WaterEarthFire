5e92fdb9f189ce6267bb8effc8af4f5564908c75
LisaMolloy5 <molloylisa5@gmail.com>
2022-04-30 17:33:01 +0000

    Add game variables

--- run.py
+++ run.py
@@ -1,13 +1,16 @@
-### Tutorial followed and changed for this project - https://www.youtube.com/watch?v=xRlN8CFJwAM&t=175s
+### Tutorial followed and changed for this project - 
 import random
 
 print("-------------------------------------------------------------------")
-print("Welcome to Water Earth Fire")
-print("A rock, paper, scissors type game based on Avatar The Last Airbender.\n Use your abilities to defeat your oponent.")
+print("Welcome to Water Earth Fire!")
+print("A rock, paper, scissors type game based on Avatar The Last Airbender.")
+print("Use your bender abilities to defeat your oponent.")
 print("--------------------------------------------------------------------")
 
-playerScore = 0
+### Game score variables
+
 computerScore = 0
+playerScore = 0
 tieScore = 0
 
 def playGame():
@@ -16,15 +19,22 @@ def playGame():
     """
     playerChoice = input("Make your choice,  Water Earth or Fire: ")
     if (playerChoice != "Water" or playerChoice != "Earth" or playerChoice != "Fire"):
-        return False
         print("Invalid choice. Please try again.")
 
     computerChoice = random.choice(['Water', 'Earth', 'Fire'])
 
     if playerChoice == computerChoice:
-        print("You and computer have both chosen", playerChoice "It,s a tie.")
-
-
+        print("You and computer have both chosen", playerChoice, "It,s a tie.")
     
+    print("Your choice: ", playerChoice, "Computer choice: ", computerChoice, "You Lost :(")
 
+def win(player, computer):
+    """
+    Function to return true if the player beats the computer.
+    The combinations 
+    """
+    if (player == "Water" and computer == "Fire") or (player == "Fire" and computer == "Earth") or (player == "Earth" and computer == "water"):
+        return True
+    return False
 
+playGame()
