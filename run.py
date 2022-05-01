4be79546de435f87207607cd854cd0fdd333ef17
LisaMolloy5 <molloylisa5@gmail.com>
2022-05-01 10:49:59 +0000

    Add how to play information

--- run.py
+++ run.py
@@ -4,8 +4,13 @@ import random
 print("-------------------------------------------------------------------")
 print("Welcome to Water Earth Fire!")
 print("A rock, paper, scissors type game based on Avatar The Last Airbender.")
-print("Use your bender abilities to defeat your oponent.")
 print("--------------------------------------------------------------------")
+print("HOW TO PLAY:")
+print("Can you use your bending abilities to defeat your opponent?")
+print("Water Beats Fire")
+print("Fire Beats Earth")
+print("Earth Beats Water")
+print("Good Luck :D")
 
 
 def hasWon(playerChoice, computerChoice):
